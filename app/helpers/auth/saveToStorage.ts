017e7ccb3f07719a23e6500927d3983abb6b3f78
osman-seitmemetov <seitmemetov228@gmail.com>
2022-12-06 02:39:14 +0300

    fixed bug

--- app/helpers/auth/saveToStorage.ts
+++
@@ -1,6 +0,0 @@
-import {LoginResponse} from "@/models/response/LoginResponse";
-
-export const saveToStorage = (data: LoginResponse) => {
-    // saveTokensStorage(data)
-    localStorage.setItem('user', JSON.stringify(data.user))
-}
