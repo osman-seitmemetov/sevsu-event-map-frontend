0492fd9f504b1f77b3ca29c79ec1e8e8397b41fa
osman-seitmemetov <seitmemetov228@gmail.com>
2023-05-22 13:54:06 +0300

    Did the little things

--- app/utils/localStorage.ts
+++ app/utils/localStorage.ts
@@ -1,7 +1,6 @@
 export const getStoreLocal = (name: string) => {
 	if (typeof localStorage !== 'undefined') {
 		const ls = localStorage.getItem(name)
-		console.log(typeof ls)
 		return ls ? JSON.parse(ls) : null
 	}
 	return null
--- app/webpages/admin/AdminUsers/AdminUsersItem/AdminUsersItem.tsx
+++ app/webpages/admin/AdminUsers/AdminUsersItem/AdminUsersItem.tsx
@@ -2,6 +2,7 @@ import React, {FC} from "react";
 import styles from "./AdminUsersItem.module.scss";
 import ButtonTransparent from "@/UI/buttons/ButtonTransparent/ButtonTransparent";
 import {IUser} from "@/models/IUser";
+import {useTypedSelector} from "@/hooks/useTypedSelector";
 
 
 interface AdminUsersItemProps {
@@ -11,6 +12,8 @@ interface AdminUsersItemProps {
 }
 
 const AdminUsersItem: FC<AdminUsersItemProps> = ({user, degrand, grand}) => {
+    const userRedux = useTypedSelector(state => state.user);
+
     return (
         <>
             <div className={styles.item}>
@@ -26,12 +29,14 @@ const AdminUsersItem: FC<AdminUsersItemProps> = ({user, degrand, grand}) => {
                             ? <ButtonTransparent
                                 style={{width: 320}}
                                 onClick={() => degrand(user.id)}
+                                disabled={user.id === userRedux.user?.id }
                             >
                                 Отнять права администратора
                             </ButtonTransparent>
                             : <ButtonTransparent
                                 style={{width: 320}}
                                 onClick={() => grand(user.id)}
+                                disabled={user.id === userRedux.user?.id }
                             >
                                 Дать права администратора
                             </ButtonTransparent>
